3e219a10d95b6cfa4943d11a7c07a932fed1a175
Rodrigo Andrade <rodrigoandrade@fb.com>
2019-07-16 13:34:34 -0700

    Added signup functionality

--- borbskeeper/Features/SignUp/SignUpViewController.m
+++ borbskeeper/Features/SignUp/SignUpViewController.m
@@ -7,16 +7,65 @@
 //
 
 #import "SignUpViewController.h"
+#import "Parse/Parse.h"
 
 @interface SignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
 @implementation SignUpViewController
 
+static NSString *const SUCCESSFUL_SIGNUP_ALERT_TITLE = @"Signup Successful";
+static NSString *const SUCCESSFUL_SIGNUP_ALERT_MESSAGE = @"Please login with your new account.";
+static NSString *const UNSUCCESSFUL_SIGNUP_ALERT_TITLE = @"Signup not successful";
+static NSString *const UNSUCCESSFUL_SIGNUP_ALERT_MESSAGE = @"Please try signing up again.";
+static NSString *const OK_ACTION_TITLE = @"OK";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (void)registerUser {
+    UIAlertController *signUpSuccessAlert = [UIAlertController alertControllerWithTitle:SUCCESSFUL_SIGNUP_ALERT_TITLE
+                                                                                message:SUCCESSFUL_SIGNUP_ALERT_MESSAGE
+                                                                         preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:OK_ACTION_TITLE
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+                                                     }];
+    [signUpSuccessAlert addAction:okAction];
+    
+    UIAlertController *signUpNotSuccessfulAlert = [UIAlertController alertControllerWithTitle:UNSUCCESSFUL_SIGNUP_ALERT_TITLE
+                                                                                      message:UNSUCCESSFUL_SIGNUP_ALERT_MESSAGE
+                                                                               preferredStyle:(UIAlertControllerStyleAlert)];
+    [signUpNotSuccessfulAlert addAction:okAction];
+    
+    PFUser *newUser = [PFUser user];
+    newUser.username = self.usernameField.text;
+    newUser.email = self.emailField.text;
+    newUser.password = self.passwordField.text;
+
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            [self presentViewController:signUpNotSuccessfulAlert animated:YES completion:nil];
+        } else {
+            [self presentViewController:signUpSuccessAlert animated:YES completion:nil];
+        };
+    }];
+}
+
+- (IBAction)didTapSignUp:(id)sender {
+    [self registerUser];
+}
+- (IBAction)didTapLogin:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)didTapSignUpView:(id)sender {
+    [self.view endEditing:YES];
 }
 
 /*
