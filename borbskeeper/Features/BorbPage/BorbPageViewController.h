f6f1fa44166310ba0575a56bb991118d162a20fd
Julia Park <juliapark628@fb.com>
2019-07-19 12:43:44 -0700

    added borb screen

---
+++ borbskeeper/Features/BorbPage/BorbPageViewController.h
@@ -0,0 +1,21 @@
+//
+//  BorbPageViewController.h
+//  borbskeeper
+//
+//  Created by juliapark628 on 7/19/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Borb.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BorbPageViewController : UIViewController
+
+@property (strong, nonatomic) Borb *borb;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
