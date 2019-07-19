035e5501ffe0aa884e5479a1fd25e91ff0f8bebb
Cassey Anene <cassanene@fb.com>
2019-07-19 10:49:25 -0700

    Added Edit Task Feature

--- borbskeeper/Features/EditTask/EditTasksViewController.h
+++ borbskeeper/Features/EditTask/EditTasksViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BorbParseManager.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
--- borbskeeper/Features/EditTask/EditTasksViewController.m
+++ borbskeeper/Features/EditTask/EditTasksViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "EditTasksViewController.h"
+#import "Task.h"
 
 @interface EditTasksViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *taskTitleTextField;
+@property (weak, nonatomic) IBOutlet UITextView *taskDescTextView;
+@property (weak, nonatomic) IBOutlet UIDatePicker *taskDeadlineDatePicker;
 
 @end
 
@@ -17,6 +21,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+- (IBAction)didTapSave:(id)sender {
+//    have to change the already made properties in the task
+    PFQuery *query = [PFQuery queryWithClassName:@"Task"];
+    // Retrieve the object by id
+    [query getObjectInBackgroundWithId:@"xWMyZ4YEGZ"
+                                 block:^(PFObject *task, NSError *error) {
+                                     // Now let's update it with some new data. In this case, only cheatMode and score
+                                     // will get sent to the cloud. playerName hasn't changed.
+                                     task[@"taskName"] = self.taskTitleTextField.text;
+                                     task[@"taskDescription"] = self.taskDescTextView.text;
+                                     task[@"dueDate"] = self.taskDeadlineDatePicker.date;
+                                     [task saveInBackground];
+                                 }];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+- (IBAction)didTapCancel:(id)sender {
+     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 /*
