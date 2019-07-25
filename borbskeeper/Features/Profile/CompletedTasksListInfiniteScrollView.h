413ed8a476c34e971acc2033b31343eac0a3e1a5
Rodrigo Andrade <rodrigoandrade@fb.com>
2019-07-25 13:13:46 -0700

    Created completed task table view within Profile Screen complete with infinite scroll functionality as well as ability to 'uncomplete' task which will return to incomplete task list. Autolayout not currently set, will do shortly.

---
+++ borbskeeper/Features/Profile/CompletedTasksListInfiniteScrollView.h
@@ -0,0 +1,17 @@
+//
+//  CompletedTasksListInfiniteScrollView.h
+//  borbskeeper
+//
+//  Created by rodrigoandrade on 7/25/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import "InfiniteScrollView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CompletedTasksListInfiniteScrollView : InfiniteScrollView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ borbskeeper/Features/Profile/CompletedTasksListInfiniteScrollView.m
@@ -0,0 +1,21 @@
+//
+//  CompletedTasksListInfiniteScrollView.m
+//  borbskeeper
+//
+//  Created by rodrigoandrade on 7/25/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import "CompletedTasksListInfiniteScrollView.h"
+
+@implementation CompletedTasksListInfiniteScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
