8639487d79c2c4e0275e702ea9ec6ac02bed514e
Rodrigo Andrade <42750527+randrade13@users.noreply.github.com>
2019-07-25 15:30:15 -0700

    Update borbskeeper/Features/Profile/ProfileViewController.m
    Co-Authored-By: Luis Mariano <lmag94@gmail.com>

--- borbskeeper/Features/Profile/ProfileViewController.m
+++ borbskeeper/Features/Profile/ProfileViewController.m
@@ -48,7 +48,7 @@ static NSString *const COMPLETE_TASK_TABLE_VIEW_CELL_ID = @"CompletedTaskCell";
     [self loadUserProfilePicture];
 }
 
--(void)loadUserProfilePicture{
+-(void)loadUserProfilePicture {
     PFFileObject *PFObjectProfileImage = [User currentUser][USER_PROF_PIC_KEY];
     NSURL *profileImageURL = [NSURL URLWithString:PFObjectProfileImage.url];
     self.profilePicture.image = nil;
