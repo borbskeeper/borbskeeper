c775e93205a389a45476c00d640ee78caf8f78ec
Julia Park <juliapark628@fb.com>
2019-07-26 13:24:55 -0700

    added compose and add-friend pages to social tab

---
+++ borbskeeper/Features/ComposePost/ComposePostViewController.h
@@ -0,0 +1,17 @@
+//
+//  ComposePostViewController.h
+//  borbskeeper
+//
+//  Created by juliapark628 on 7/26/19.
+//  Copyright © 2019 juliapark628. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposePostViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
